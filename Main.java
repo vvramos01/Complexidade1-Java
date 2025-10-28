1df87d78d336757e402d178fab819a395b016638
Vitória Ramos <vitoria.ramos@edu.unifil.br>
2025-10-28 19:38:35 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ManipulaVetor manipulaVetor = new ManipulaVetor();
+        manipulaVetor.lerVetor();
+        manipulaVetor.imprimirVetor();
+        manipulaVetor.insertionSort();
+        manipulaVetor.imprimirVetor();
+    }
+}
